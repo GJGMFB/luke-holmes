abc85f38a4dccded8da86712a3551811e8a423f1
Dennis Aurelian Leancu <dennis.leancu@gmail.com>
2017-03-16 05:36:43 -0500

    Add files via upload

---
+++ Assets/quest1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class quest1 : MonoBehaviour {
+	public GameObject gate;
+	public GameObject t;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if (PlayerPrefs.HasKey ("item1") && PlayerPrefs.GetInt ("item1") == 1) {
+		if (globals.Item) {
+			var te = t.GetComponent<TextMesh> ();
+			te.text = "Thanks for finding the key! Now the gate is open!";
+			Destroy (gate);
+		}
+	}
+}
